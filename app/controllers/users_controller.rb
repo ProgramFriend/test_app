3a0af107941cf57778a15ed3aeb3e3d57bb0a510
Rails Udemy Course <edvardasras@gmail.com>
2017-01-09 17:05:01 +0000

    Admin update!

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:edit, :update, :show]
-  before_action :require_same_user, only: [:edit, :update]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
+  before_action :require_admin, only: [:destroy]
   
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
@@ -12,6 +13,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = "Welcome to alpha blog #{@user.username}!"
       redirect_to user_path(@user)
     else
@@ -35,6 +37,13 @@ class UsersController < ApplicationController
     @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
   
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    flash[:danger] = "User and all user's articles was deleted"
+    redirect_to users_path
+  end
+  
   def set_user
     @user = User.find(params[:id])
   end
@@ -43,8 +52,15 @@ class UsersController < ApplicationController
     params.require(:user).permit(:username, :email, :password)
   end
   def require_same_user
-    if current_user != @user
+    if current_user != @user and !current_user.admin
       flash[:danger] = "You can edit just your account"
+      redirect_to root_path
+    end
+  end
+  
+  def require_admin
+    if logged_in? and !current_user.admin
+      flash[:danger] = "Only admin can access his action"
       redirect_to root_path
     end
   end
